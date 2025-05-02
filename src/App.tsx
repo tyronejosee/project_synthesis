0f7bd5804d7c6b1e04daa72c41680a25580369dd
tyronejose <alt.tyronejose@gmail.com>
2025-05-01 23:25:01 -0400

    feat: implement gridstack integration with context and rendering support

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Calculator from "./components/features/calculator/calculator";
+import { GridStackDemo } from "./components/common";
 
 function App() {
   return (
     <>
       <h1 className="text-6xl font-bold">Project Synthesis</h1>
+      <GridStackDemo />
       <Calculator />
     </>
   );
